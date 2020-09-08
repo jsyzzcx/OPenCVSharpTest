56209912b64d851b8c6e3eb732d04ba59ff8d2c4
zhaocx <zhaocx@gtintel.cn>
2020-09-08 11:01:40 +0800

    灰度图

---
+++ OpenCVSharpTest/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace OpenCVSharpTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.PictureBox_source = new System.Windows.Forms.PictureBox();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.PictureBox_target = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_source)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_target)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Items.AddRange(new object[] {
+            "转化为灰度图",
+            "反色",
+            "图像二值化",
+            "高斯滤波",
+            "均值滤波",
+            "中值滤波",
+            "双边滤波",
+            "边缘检测(sobel)",
+            "边缘检测(scharr)",
+            "边缘检测(canny)",
+            "边缘检测(Laplacian)",
+            "浮雕效果",
+            "反转（左右）",
+            "反转（上下）",
+            "毛玻璃效果",
+            "锐化",
+            "膨胀",
+            "腐蚀",
+            "顶帽",
+            "黑帽",
+            "gamma暗部增强",
+            "log亮部增强",
+            "显示直方图",
+            "轮廓绘制",
+            "模板匹配",
+            "绘制轮廓图"});
+            this.listBox1.Location = new System.Drawing.Point(2, 1);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(160, 498);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // PictureBox_source
+            // 
+            this.PictureBox_source.Location = new System.Drawing.Point(168, 44);
+            this.PictureBox_source.Name = "PictureBox_source";
+            this.PictureBox_source.Size = new System.Drawing.Size(524, 449);
+            this.PictureBox_source.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox_source.TabIndex = 9;
+            this.PictureBox_source.TabStop = false;
+            // 
+            // openBtn
+            // 
+            this.openBtn.AutoSize = true;
+            this.openBtn.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.openBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.openBtn.Location = new System.Drawing.Point(168, 1);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(94, 30);
+            this.openBtn.TabIndex = 10;
+            this.openBtn.Text = "打开图片";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // PictureBox_target
+            // 
+            this.PictureBox_target.Location = new System.Drawing.Point(700, 44);
+            this.PictureBox_target.Name = "PictureBox_target";
+            this.PictureBox_target.Size = new System.Drawing.Size(524, 449);
+            this.PictureBox_target.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox_target.TabIndex = 11;
+            this.PictureBox_target.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 504);
+            this.Controls.Add(this.PictureBox_target);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.PictureBox_source);
+            this.Controls.Add(this.listBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_source)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_target)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox PictureBox_source;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.PictureBox PictureBox_target;
+    }
+}
+
---
+++ OpenCVSharpTest/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenCVSharpTest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public Mat pictureSourceMat;//当前主窗体图像
+        public int selectIndex;
+        public enum lb1_list//记录操作方法
+        {
+            /// <summary>
+            /// 灰度
+            /// </summary>
+            gray,
+            reverse,
+            binary,
+            gaussianBlur,
+            blur,
+            medianBlur,
+            bilateralFilter,
+            sobel,
+            scharr,
+            canny,
+            Laplacian,
+            carve,
+            rever_lr,
+            rever_tb,
+            frost,
+            sharp,
+            dilate,
+            erode,
+            tophat,
+            blackhat,
+            gamma,
+            log,
+            hist,
+            findContours,
+            template,
+            drawRect,
+
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd1 = new OpenFileDialog();//创建打开文件夹窗体
+            ofd1.Title = "请选择输入的图像";
+            ofd1.InitialDirectory = @"";//初始化目录
+            ofd1.Multiselect = false;//不可多选
+            ofd1.Filter = "图像文件|*.jpg;*.png;*.bmp|全部文件|*.*";
+            ofd1.ShowDialog();
+
+            if (ofd1.FileName != string.Empty)
+            {
+                try
+                {
+                    this.PictureBox_source.Load(ofd1.FileName);
+                    pictureSourceMat = new Mat(ofd1.FileName);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("打开失败，请检查文件格式是否符合");
+                }
+
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            selectIndex = listBox1.SelectedIndex;
+            handle(selectIndex);
+        }
+
+        public void handle(int selectIndex)
+        {
+            switch (selectIndex)
+            {
+                case (int)lb1_list.gray:
+                    if (pictureSourceMat.Channels() == 3)
+                    {
+                        Cv2.CvtColor(pictureSourceMat, pictureSourceMat, ColorConversionCodes.BGR2GRAY);
+                        this.PictureBox_target.Image = pictureSourceMat.ToBitmap();
+                    }
+                    else
+                    {
+                        MessageBox.Show("已是灰度图像，不要重复操作");
+                    }
+                    break;
+ 
+                default:
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
